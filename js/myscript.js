0e1a59cde133dc3333e66c1409f56c265b3800a3
IGORVK <igor.haletskiy@gmail.com>
2016-03-23 21:01:23 +0200

    fixed with Date() but Pause is trouble

--- js/myscript.js
+++ js/myscript.js
@@ -1,46 +1,53 @@
 
 var active = false;
+var msStart;
+var msRunning;
+var diff;
+var memoryStart;
+
 
 
 function start_timer(){
-
-  if (active){
-    var timer = document.getElementById("my_timer").innerHTML;
-    var arr = timer.split(":");
-    var hour = arr[0];
-    var min = arr[1];
-    var sec = arr[2];
-
-    if (sec == 59) {
-      if(min == 59) {
-        hour++;
-        min = 0;
-        if(hour < 10) hour = "0" + hour;
-      }else {
-        min++;
-      }
-      if(min < 10 ) min = "0" + min;
-      sec = 0;
-    } else {
-      sec++;
-      if (sec < 10) sec = "0" + sec;
-    }
-
-    document.getElementById("my_timer").innerHTML = hour + ':' + min + ':' + sec;
-    setTimeout(start_timer, 1000); 
-  }
+    
+    
+    if (active){
+        
+        msRunning = new Date().getTime();
+         
+         diff  = msRunning - msStart;
+         
+         var miliSec = new Date(diff).getUTCMilliseconds(); 
+         var sec = new Date(diff).getUTCSeconds(); 
+         var min = new Date(diff).getUTCMinutes();
+         var hours = new Date(diff).getUTCHours();
+            
+        
+          if(hours < 10) hours = "0" + hours;
+              
+          if(min < 10 ) min = "0" + min;
+         
+          if(sec < 10) sec = "0" + sec;
+      
+         document.getElementById("ms").innerHTML = miliSec;
+         document.getElementById("my_timer").innerHTML = hours + ':' + min + ':' + sec;
+         setTimeout(start_timer, 1); 
+         }
+    
 
 };
 
 
 function changeState() {
   if (active == false) {
+    
     active = true;
+    msStart = new Date().getTime();
     start_timer();
     console.log('Timer has been started');
     document.getElementById("control").innerHTML = "PAUSE";
   }else {
     active = false;
+    
     console.log('Timer is on pause');
     document.getElementById('control').innerHTML = "START";
   }
@@ -49,6 +56,10 @@ function changeState() {
   
 function reset() {
       document.getElementById("my_timer").innerHTML = "00" + ':' + "00" + ':' + "00";
+      document.getElementById("ms").innerHTML = "0";
+      
+      
+      
       console.log("Timer has been reset");
 };
 
